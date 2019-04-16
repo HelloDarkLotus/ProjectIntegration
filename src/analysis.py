44b52e9c64b2fa339b887fd2000fa56cb35773b8
HelloDarkLotus <44580711+HelloDarkLotus@users.noreply.github.com>
2019-04-16 23:37:20 +0800

    new create two classes
    new create two classes for spider data and data analysis

---
+++ src/analysis.py
@@ -0,0 +1,30 @@
+import csv
+
+class DataAnalysis(object):
+    def __init__(self, csvFile):
+        self.file = csvFile
+        self.stsTbl = dict()
+        self.totalNum = 0
+
+    def GenDataDict(self):
+        with open(self.file) as f:
+            handler = csv.reader(f)
+            next(handler)
+            self.totalNum = len(list(handler))
+            for row in handler:
+                if 'New' in row:
+                    self.stsTbl[r'New'] += 1
+                elif 'Assigned' in row:
+                    self.stsTbl[r'Assigned'] += 1
+                elif 'Working' in row:
+                    self.stsTbl[r'Working'] += 1
+                elif 'Resolved' in row:
+                    self.stsTbl[r'Resolved'] += 1
+                elif 'Rejected' in row:
+                    self.stsTbl[r'Rejected'] += 1
+                elif 'Verified' in row:
+                    self.stsTbl[r'Verified'] += 1
+                elif 'Closed' in row:
+                    self.stsTbl[r'Closed'] += 1
+                else:
+                    self.stsTbl[r'Pending'] += 1
---
+++ src/pipeline.py
@@ -0,0 +1,113 @@
+import os
+import configparser
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+class PipeLine(object):
+    def __init__(self):
+        self.userName = ""
+        self.passWord = ""
+        self.versionNumber = ""
+        self.projectName = ""
+        self.baseUrl = ""
+        self.basePath = ""
+        self.targetPath = ""
+        self.gitRepo = ""
+        self.session = ""
+        self.headers = ""
+
+    #return configure file's path
+    def GetConfigPath(self):
+        return os.getcwd() + r'\config.ini'
+
+    #parser configure file than get configure parameters
+    def ConfigMethod(self, fileName):
+        config = configparser.ConfigParser()
+        config.read(fileName)
+        self.userName = config.get("RedmineInfo", "username")
+        self.passWord = config.get("RedmineInfo", "password")
+        self.baseUrl = config.get("RedmineInfo", "baseurl")        
+        self.versionNumber = config.get("ProjectInfo", "version")
+        self.projectName = config.get("ProjectInfo", "projname")
+        self.basePath = config.get("CompileInfo", "basepath")
+        self.targetPath = config.get("CompileInfo", "targetpath")
+        self.gitRepo = config.get("CompileInfo", "gitrepo")
+
+    #generate HTTP request header
+    def GenHeader(self):
+        accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+        accept_encoding = 'gzip,deflate,sdch'
+        accept_language = 'zh-CN,zh;q=0.8'
+        user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36'
+
+        self.headers = {
+            'Accept' : accept,
+            'Accept-Encoding' : accept_encoding,
+            'Accept-Language' : accept_language,
+            'User-Agent' : user_agent,
+        }
+
+    #get token string for login authentication
+    def GetLoginToken(self):
+        loginUrl = self.baseUrl + r'login'
+        self.session = requests.Session()
+        r = self.session.get(loginUrl, headers=self.headers)
+        soup = BeautifulSoup(r.text, 'lxml')
+        return soup.find('input', {'name' : 'authenticity_token'})['value']
+
+    #login redmine
+    def LoginMethod(self):
+        token = self.GetLoginToken()
+        if token != None:
+            post_data = {
+                'username' : self.userName,
+                'password' : self.passWord,
+                'authenticity_token' : token,
+            }
+
+            #because redmine login successful will redirect, so should forbidden it
+            r = self.session.post(self.baseUrl + r'login', data=post_data, headers=self.headers, allow_redirects=False)
+
+    #spider bug information and store into list
+    def SpiderMethod(self):
+        roadmapUrl = self.baseUrl + r'projects/' + self.projectName.lower() + r'/roadmap'
+        r = self.session.get(roadmapUrl)
+        soup = BeautifulSoup(r.text, 'lxml')
+        href = soup.find('a', {'name' : self.versionNumber})['href']
+        siUrl = r'http://192.168.100.103' + href
+
+        #get all bug information
+        r = self.session.get(siUrl)
+        soup = BeautifulSoup(r.text, 'lxml')
+        bugNumLst = list()
+        for issue in soup.find_all('tr', {'class' : 'issue hascontextmenu'}):
+            bugNumLst.append(issue.find('input')['value'])
+
+        #enter into each bug tickets then get its information
+        bugInfo = list()
+        for bugNum in bugNumLst:
+            url = self.baseUrl + r'issues/' + bugNum
+            r = self.session.get(url)
+            soup = BeautifulSoup(r.text, 'lxml')
+            #bug title
+            bugTtl = soup.find('div', {'class' : 'subject'}).find('h3').text
+            #bug status
+            bugSts = soup.find('div', {'class' : 'status attribute'}).find('div', {'class' : 'value'}).text
+            #bug processor
+            bugPrssr = soup.find('div', {'class' : 'assigned-to attribute'}).find('a', {'class' : 'user active'}).text
+            #bug process start time
+            bugStrt = soup.find('div', {'class' : 'start-date attribute'}).find('div', {'class' : 'value'}).text
+            #bug progress
+            bugPrgrss = soup.find('div', {'class' : 'progress attribute'}).find('p', {'class' : 'percent'}).text
+            
+            bugInfo.append((bugNum, bugTtl, bugSts, bugPrssr, bugStrt, bugPrgrss))
+
+        return bugInfo
+
+    def StoreData(self, *kargs):
+        with open('tmp.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([r'Redmine票号', r'问题标题', r'问题状态', r'问题处理人', r'开始时间', r'处理进度'])
+            for bugItem in kargs:
+                writer.writerows([bugItem])
