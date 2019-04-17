6468bed2be17112150c2d1b947bf27a1d7075e52
HelloDarkLotus <44580711+HelloDarkLotus@users.noreply.github.com>
2019-04-17 23:26:34 +0800

    new add class for cross compile
    add a new class to implement cross compile

---
+++ src/compile.py
@@ -0,0 +1,24 @@
+import paramiko
+from git import Repo
+
+class CompileModule(object):
+    def __init__(self, ip, username, password):
+        self.Ssh = ""
+        self.ServerIP = ip
+        self.UserName = username
+        self.PassWord = password
+
+    def ConnectServer(self):
+        self.Ssh = paramiko.SSHClient()
+        self.Ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.Ssh.connect(self.ServerIP, 22, self.UserName, self.PassWord)
+
+    def ExecuteCommand(self, cmd):
+        stdout = self.Ssh.exec_command(cmd)
+        print(stdout.readlines())
+
+    def CloseConnection(self):
+        self.Ssh.close()
+
+    def CloneCodeFromGit(self, repoPath, srcPath):
+        Repo.clone_from(repoPath, srcPath)
